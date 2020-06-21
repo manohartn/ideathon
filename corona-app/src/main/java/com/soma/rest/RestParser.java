06371e57d1f978f049548c9222e881098a180fe4
ananth.reddipalli <ananth.reddipalli@gmail.com>
2020-06-21 08:43:00 +0100

    Added RestParser class file

--- corona-app/src/main/java/com/soma/rest/RestParser.java
+++ corona-app/src/main/java/com/soma/rest/RestParser.java
@@ -1,5 +1,44 @@
 package com.soma.rest;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.net.*;
+
 public class RestParser {
+	
+	public String getIndCaronaDts(String urlInfo){
+		HttpURLConnection conn = null;
+		String inline = "";
+		try{
+			URL url = new URL(urlInfo);
+			conn = (HttpURLConnection)url.openConnection();
+			conn.connect();
+			int responseCode = conn.getResponseCode();
+			if (responseCode != 200)
+			throw new RuntimeException("HttpResponseCode: "+responseCode);
+			else
+			{
+				Scanner sc = new Scanner(url.openStream());
+				while(sc.hasNext()){
+					inline += sc.nextLine();
+				}
+				sc.close();
+			}
+		} catch (IOException e){
+			e.printStackTrace();
+		} finally {
+			if (conn != null)
+			{
+				conn.disconnect();
+			}
+		}
+		return inline;
+	}
+
+	public static void main(String[] args) {
+		RestParser rstParser = new RestParser();
+		String indCovidData = rstParser.getIndCaronaDts("https://api.covid19india.org/raw_data1.json");
+		System.out.println(indCovidData);
+	}
 
 }
